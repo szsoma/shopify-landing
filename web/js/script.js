3563c828d9610721c16dd97d38b2168731fc6d81
Soma Szoboszlai <soma.szoboszlai@innonic.com>
2018-05-24 15:16:01 +0200

    init

---
+++ web/js/script.js
@@ -0,0 +1,2 @@
+function openCity(e,t,n){var l,o,a;for(o=document.getElementsByClassName("tabcontent"),l=0;l<o.length;l++)o[l].style.display="none";for(a=document.getElementsByClassName("tablink"),l=0;l<a.length;l++)a[l].style.backgroundColor="";document.getElementById(e).style.display="block",t.style.backgroundColor=n}document.getElementById("defaultOpen").click();
+//# sourceMappingURL=maps/script.js.map
